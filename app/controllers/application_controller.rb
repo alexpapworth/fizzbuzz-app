e69b01e80b16694c53abd138b46009a6c5d7263b
Alex Papworth <arpapworth@gmail.com>
2019-06-29 10:54:36 +0100

    Add user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	
+	before_action :current_user
+
+	private
+		def current_user
+			@current_user ||= Session.find_by_auth_token(cookies[:auth_token]).user if cookies[:auth_token]
+		end
+
+		helper_method :current_user
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+  def new
+  	@current_user = User.create(name: helpers.generate_username)
+  	generate_token(@current_user)
+  end
+
+  private
+  	def generate_token(user)
+			begin
+				token = SecureRandom.urlsafe_base64 + SecureRandom.urlsafe_base64 + SecureRandom.urlsafe_base64 + SecureRandom.urlsafe_base64
+				user.sessions.create(auth_token: token)
+				cookies.permanent[:auth_token] = token
+			end unless Session.find_by_auth_token( token )
+		end
+end
