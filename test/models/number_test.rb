98ca18394020299bb24b1c0702cecf0283fc8fcd
Alex Papworth <arpapworth@gmail.com>
2019-07-05 11:06:26 +0100

    Fix test for blank Result

--- test/models/number_test.rb
+++ test/models/number_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class NumberTest < ActiveSupport::TestCase
-  test "should not save number without fizzbuzz result" do
-    number = Number.new
-  	assert_not number.save, "Saved the number without a fizzbuzz result"
+  test "should save number with blank fizzbuzz result" do
+    number = Number.new( id: 111, result: "" )
+    assert number.save, "Couldn't save the number with a blank result"
   end
 end
